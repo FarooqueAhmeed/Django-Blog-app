1acd738702147aa518cd37126a11e846bb3adc9e
zizopixels <zizopixels11@gmail.com>
2021-01-05 16:40:32 +0500

    init

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 from blog.models import Blog
 
@@ -14,3 +16,13 @@ class Blog_update_Form(ModelForm):
     class Meta:
         model = Blog
         fields = ['title','content']
+
+
+
+
+class SignUpForm(UserCreationForm):
+    useremail = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ['username','useremail', 'password1', 'password2']
