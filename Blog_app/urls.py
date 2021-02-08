7366da778bd7fc4293fe7e8838b56136598671d6
zizopixels <62591655+zizopixels@users.noreply.github.com>
2021-02-08 19:05:58 +0500

    Update urls.py

--- Blog_app/urls.py
+++ Blog_app/urls.py
@@ -73,7 +73,7 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns  += [
 
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
