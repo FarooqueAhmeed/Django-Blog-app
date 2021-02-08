41fcca402609c808d261e7bca0367f6d06315c50
zizo-pixels <zizopixels11@gmail.com>
2021-02-08 13:09:30 +0500

    init

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user          = models.OneToOneField(User,on_delete=models.CASCADE)
-    avatar = models.ImageField(null=True, blank=True,upload_to="profile_pics",default='/media/profile.png')
+    avatar = models.ImageField(null=True, blank=True,upload_to="profile_pics",default='profile.png')
     def __str__(self):
         return '{} {} {}'.format(self.user,'-image-', self.avatar)
 
