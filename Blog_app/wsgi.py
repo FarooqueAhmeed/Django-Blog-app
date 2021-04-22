40c843404bf4413f9072542d232b370117381ed7
zizopixels <zizopixels11@gmail.com>
2021-04-22 03:50:50 -0700

    V

--- Blog_app/wsgi.py
+++ Blog_app/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog_app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Blog_app.settings")
 
 application = get_wsgi_application()
+
+if not settings.DEBUG:
+    try:
+        from dj_static import Cling
+        application = Cling(get_wsgi_application())
+    except:
+        pass
--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 release: python manage.py migrate 
-#web: gunicorn : Blog_app.wsgi --log-file -
+web: gunicorn : Blog_app.wsgi --log-file -
 web: gunicorn Blog_app.config.settings.wsgi
 
 web: gunicorn Blog_app.wsgi:application --log-file -
