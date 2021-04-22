2156f74e9550108586cb7bf115baed37dee7e1e7
zizopixels <62591655+zizopixels@users.noreply.github.com>
2021-04-22 08:18:40 -0700

    Update settings.py

--- Blog_app/settings.py
+++ Blog_app/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'o&cq0vj-qp$08lx24@a7m*(g_mny$fzxvva=*#b9#(p0+a(mvf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','zizoblogs.herokuapp.com']
